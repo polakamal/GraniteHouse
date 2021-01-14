a39f0a77bbbfc89d1c23e5f5b4f7a3340b9c9f99
polakamal <polakamal82@gmail.com>
2021-01-14 18:43:14 +0200

    Add project files.

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductsTypesController.cs
@@ -0,0 +1,43 @@
+﻿using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductsTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.productTypes.ToList());
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes ProductTypes) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Add(ProductTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            }
+            return View(ProductTypes);
+        }
+        
+    }
+}
---
+++ GraniteHouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GraniteHouse.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraniteHouse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductTypes>  productTypes { get; set; }
+    }
+}
